bc43b8815a39e34cadaa87f0dd04c20f684be183
elliscode <45629640+elliscode@users.noreply.github.com>
2026-06-30 19:39:41 -0400

    Excluding files that dont need to be in the package

--- frontend-v3/kaios-release.sh
+++ frontend-v3/kaios-release.sh
@@ -1,2 +1,12 @@
 TIMESTAMP=$(date +%s)
-zip -r sharedlist-${TIMESTAMP}.zip . -x "*.zip" -x *.DS_Store -x *release.sh
+zip -r sharedlist-${TIMESTAMP}.zip . \
+  -x "*.zip" \
+  -x "*.DS_Store" \
+  -x "*.md" \
+  -x "*release.sh" \
+  -x "assets/banner.png" \
+  -x "assets/banner_large.png" \
+  -x "assets/screenshot01.png" \
+  -x "assets/favicon.png" \
+  -x "assets/apple-touch-icon.png" \
+  -x "assets/share.svg"
